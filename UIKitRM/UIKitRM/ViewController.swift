3443fdc3bc816a1f0152fd1b5b9dd8f95bf6b43c
Natalia_Poftalnaya <poftnataly@gmail.com>
2024-02-07 21:53:59 +0300

    Initial Commit

---
+++ UIKitRM/UIKitRM/ViewController.swift
@@ -0,0 +1,12 @@
+// ViewController.swift
+// Copyright © RoadMap. All rights reserved.
+
+import UIKit
+
+/// Стартовый экран приложения
+class ViewController: UIViewController {
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        // Do any additional setup after loading the view.
+//    }
+}
