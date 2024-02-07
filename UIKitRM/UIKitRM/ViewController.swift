26c7d5aad5251068d9fa826ba3994434847d5cd2
Natalia_Poftalnaya <poftnataly@gmail.com>
2024-02-07 23:35:55 +0300

    Коммит 07022024

--- UIKitRM/UIKitRM/ViewController.swift
+++ UIKitRM/UIKitRM/ViewController.swift
@@ -5,8 +5,23 @@ import UIKit
 
 /// Стартовый экран приложения
 class ViewController: UIViewController {
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        // Do any additional setup after loading the view.
-//    }
+    private lazy var greetingLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont(name: "Roboto", size: 30)
+        label.textColor = .white
+        label.frame = CGRect(x: 0, y: 37, width: view.frame.width, height: 82)
+        label.backgroundColor = UIColor(red: 151 / 255, green: 203 / 255, blue: 229 / 255, alpha: 1)
+        return label
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func setupUI() {
+        view.b
+        view.addSubview(greetingLabel)
+
+
+    }
 }
